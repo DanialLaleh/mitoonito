8ae2cdd46abb48733fffab05e95fecce13564633
DanialLaleh <danial.laleh@gmail.com>
2026-07-31 14:43:38 +0330

    Update design-tokens.ts

--- src/lib/design-tokens.ts
+++ src/lib/design-tokens.ts
@@ -1,8 +1,12 @@
-// src/lib/design-tokens.ts
-export const defaultAreas = [
-  { name: "فیزیک", icon: "💪" },
-  { name: "فوتبال", icon: "⚽" },
-  { name: "کار", icon: "💼" },
-  { name: "پول", icon: "💰" },
-  { name: "زندگی شخصی", icon: "🏠" },
-];
+export const colors = {
+  white: "#FFFFFF",
+  gray: "#E6E7E8",
+  darkGray: "#434345",
+  lightGreen: "#9FD18B",
+  green: "#50B848",
+  darkGreen: "#367639",
+} as const;
+
+export const designTokens = {
+  colors,
+} as const;
